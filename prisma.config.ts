3e008ef84de39a4b482d0b3b0f79b1d7263e2385
FindMalek <hi@findmalek.com>
2025-10-22 20:39:47 +0100

    perf: updated 'prisma' client

---
+++ prisma.config.ts
@@ -0,0 +1,19 @@
+import "dotenv/config"
+import path from "node:path"
+import { defineConfig, env } from "prisma/config"
+
+type Env = {
+  DATABASE_URL: string
+}
+
+export default defineConfig({
+  schema: path.join("prisma", "schema"),
+  migrations: {
+    path: path.join("prisma", "schema", "migrations"),
+    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed/index.ts",
+  },
+  engine: "classic",
+  datasource: {
+    url: env<Env>("DATABASE_URL"),
+  },
+})
