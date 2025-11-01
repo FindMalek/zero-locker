f45f2d6be07e3ab8e1c98788ac0f9d0988102543
FindMalek <hi@findmalek.com>
2025-11-02 01:43:28 +0300

    fix: resolved 'build' errors

--- entities/subscription/transaction/query.ts
+++ entities/subscription/transaction/query.ts
@@ -24,7 +24,7 @@ export class TransactionQuery {
         include: SubscriptionQuery.getSimpleInclude(),
       },
       invoice: {
-        select: InvoiceQuery.getSimpleSelect(),
+        include: InvoiceQuery.getSimpleInclude(),
       },
     } satisfies Prisma.PaymentTransactionInclude
   }
